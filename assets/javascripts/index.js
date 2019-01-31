4f2c406e33dae3f663e02c803cf953da20f7c971
Andrew Lawton <andrew@andrewlawton.com>
2019-01-31 15:49:29 -0500

    Adding sign-in and forgot password design, alert related script and styles

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -107,3 +107,11 @@ $(function() {
       source: availableTags
     });
 });
+
+// alerts
+
+$(function() {
+  $('#reset-password').click(function() {
+    $('#alert-reset-password').fadeIn("fast").delay(2000).fadeOut("fast");
+  });
+});
