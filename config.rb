1f02ded439476f94f25521f191363c38f93b9877
Andrew Lawton <andrew@andrewlawton.com>
2020-10-17 14:24:50 -0400

    Adding imgage_set_tag helper method to support responsive images

--- config.rb
+++ config.rb
@@ -51,6 +51,20 @@ page '/*.txt', layout: false
 #   end
 # end
 
+helpers do
+  # Acts as a thin wrapper for image_tag and generates a srcset attribute for regular image tags
+  # for usage with responsive images, supports middleman's asset_path helper.
+  #
+  # image_set_tag 'pic_1980.jpg', { 'pic_640.jpg' => '640w', 'pic_1024.jpg' => '1024w', 'pic_1980.jpg' => '1980w' }, sizes: '100vw', class: 'my-image'
+  #
+  # => <img src="/assets/ants_1980.jpg" srcset="/assets/pic_640.jpg 640w, /assets/pic_1024.jpg 1024w, /assets/pic_1980.jpg 1980w" sizes="100vw" class="my-image">
+  #
+  def image_set_tag(source, srcset = {}, options = {})
+    srcset = srcset.map { |src, size| "#{asset_path(:images, src)} #{size}" }.join(', ')
+    image_tag(source, options.merge(srcset: srcset))
+  end
+end
+
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
